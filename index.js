9018946b9fb1dfe81f7dea02e69a2c266c3f2828
Jitesh2Git <themastermind2003rv@gmail.com>
2024-06-13 10:15:41 +0530

    changes

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   })
 );
+
+app.get("/", (req, res) => {
+  res.status(200).send("Backend Running.");
+});
 
 //Create Account
 app.post("/create-account", async (req, res) => {
