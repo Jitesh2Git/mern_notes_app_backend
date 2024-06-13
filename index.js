c28aa07b5b771ea2d7412922ffb9cbb033ef418f
Jitesh2Git <themastermind2003rv@gmail.com>
2024-06-13 16:05:30 +0530

    changes

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ mongoose.connect(connectionString);
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    "Access-Control-Allow-Origin": "*",
-  })
-);
-
 app.get("/", (req, res) => {
   res.status(200).send("Backend Running.");
 });
